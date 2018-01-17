3e1cd1fae32e93b5395ff4dc4e1126a5862f1bd0
Felix Schütt <felix.schuett@maps4print.com>
2018-01-17 23:32:59 +0100

    Added basic CSS parser (CSS rule extraction)

--- src/css.rs
+++ src/css.rs
@@ -9,64 +9,134 @@ const NATIVE_CSS_LINUX: &str = include_str!("../assets/native_linux.css");
 #[cfg(target_os="macos")]
 const NATIVE_CSS_MACOS: &str = include_str!("../assets/native_macos.css");
 
+#[derive(Debug)]
 pub struct Css {
-
+	pub rules: Vec<CssRule>,
 }
 
 #[derive(Debug)]
-pub struct CssParseError {
+pub enum CssParseError {
+	ParseError(::simplecss::Error),
+	UnclosedBlock,
+	MalformedCss,
+}
 
+#[derive(Debug)]
+pub struct CssRule {
+	/// `div` (`*` by default)
+	pub html_type: String,
+	/// `#myid` (`*` by default)
+	pub id: String,
+	/// `.myclass .myotherclass` (vec![] by default)
+	pub classes: String,
+	/// `("justify-content", "center")` (vec![] by default)
+	pub declaration: (String, String),
 }
 
 impl Css {
 	pub fn new() -> Self {
 		Self {
-
+			rules: Vec::new(),
 		}
 	}
 
-	pub fn parse_from_string(css_string: &str) -> Result<Self, CssParseError> {
+	pub fn new_from_string(css_string: &str) -> Result<Self, CssParseError> {
 		use simplecss::{Tokenizer, Token};
 
-		println!("{:?}", css_string);
-
 		let mut tokenizer = Tokenizer::new(css_string);
+
+		let mut block_nesting = 0_usize;
+		let mut css_rules = Vec::<CssRule>::new();
+
+		// TODO: For now, rules may not be nested, otherwise, this won't work
+		// TODO: This could be more efficient. We don't even need to clone the
+		// strings, but this is just a quick-n-dirty CSS parser
+		// This will also use up a lot of memory, since the strings get duplicated
+
+		let mut parser_in_block = false;
+		let mut current_type = "*";
+		let mut current_id = "*";
+		let mut current_classes = Vec::<String>::new();
 
 		'css_parse_loop: loop {
 			let tokenize_result = tokenizer.parse_next();
 			match tokenize_result {
 				Ok(token) => {
-					println!("got token - {:?}", token);
-					if token == Token::EndOfStream {
-						break 'css_parse_loop;
+					match token {
+						Token::EndOfStream => {
+							break 'css_parse_loop;
+						},
+						Token::BlockStart => {
+							parser_in_block = true;
+							block_nesting += 1;
+						},
+						Token::BlockEnd => {
+							block_nesting -= 1;
+							parser_in_block = false;
+							current_type = "*";
+							current_id = "*";
+							current_classes = Vec::<String>::new();
+						},
+						Token::TypeSelector(id) => {
+							if !parser_in_block {
+								current_type = id;
+							} else {
+								return Err(CssParseError::MalformedCss);
+							}
+						},
+						Token::ClassSelector(class) => {
+							if !parser_in_block {
+								current_classes.push(class.to_string());
+							} else {
+								return Err(CssParseError::MalformedCss);
+							}
+						}
+						Token::Declaration(key, val) => {
+							if parser_in_block {
+								css_rules.push(CssRule {
+									html_type: current_type.to_string(),
+									id: current_id.to_string(),
+									classes: current_classes.drain(..).collect(),
+									declaration: (key.to_string(), val.to_string()),
+								})
+							} else {
+								return Err(CssParseError::MalformedCss);
+							}
+						},
+						_ => { }
 					}
 				},
 				Err(e) => {
 					print_simplecss_error(e);
-					return Err(CssParseError { })
+					return Err(CssParseError::ParseError(e));
 				}
 			}
 		}
 
-		Ok(Self {
+		// non-even number of blocks
+		if block_nesting != 0 {
+			return Err(CssParseError::UnclosedBlock);
+		}
 
+		Ok(Self {
+			rules: css_rules,
 		})
 	}
 
 	/// Returns the native style for the OS
 	#[cfg(target_os="windows")]
 	pub fn native() -> Self {
-		Self::parse_from_string(NATIVE_CSS_WINDOWS).unwrap()
+		Self::new_from_string(NATIVE_CSS_WINDOWS).unwrap()
 	}
 
 	#[cfg(target_os="linux")]
 	pub fn native() -> Self {
-		Self::parse_from_string(NATIVE_CSS_LINUX).unwrap()
+		Self::new_from_string(NATIVE_CSS_LINUX).unwrap()
 	}
 
 	#[cfg(target_os="macos")]
 	pub fn native() -> Self {
-		Self::parse_from_string(NATIVE_CSS_MACOS).unwrap()
+		Self::new_from_string(NATIVE_CSS_MACOS).unwrap()
 	}
 }
 
