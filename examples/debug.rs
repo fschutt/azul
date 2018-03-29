312e5b5e12624ded64dba8f399e3b949ffc55056
Felix Schütt <felix.schuett@maps4print.com>
2018-03-30 01:37:29 +0200

    Break text into lines on word boundaries

--- examples/debug.rs
+++ examples/debug.rs
@@ -21,7 +21,7 @@ impl LayoutScreen for MyAppData {
         dom.event(On::MouseUp, Callback::Sync(my_button_click_handler));
         
         dom.add_sibling(Dom::new(NodeType::Label { 
-            text: String::from("Azul App"),
+            text: String::from("This is a very long string that should break into multiple lines"),
         }));
 
         dom
