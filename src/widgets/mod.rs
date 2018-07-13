3f869f2fc89158c2f3d188f820b8d2f3a6e268aa
Felix Schütt <felix.schuett@maps4print.com>
2018-07-13 17:14:20 +0200

    Refactored widgets into their own files
    Merged SVG widget and implementation because it's annoying constantly having to switch back and forth between the two files to adjust shaders

---
+++ src/widgets/mod.rs
@@ -0,0 +1,8 @@
+pub mod svg;
+pub mod button;
+pub mod label;
+
+// Re-export widgets
+pub use self::svg::{Svg, SvgLayerId, SvgLayer, SvgCache};
+pub use self::button::{Button, ButtonContent};
+pub use self::label::Label;
