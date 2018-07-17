db892e3f52157e78b0c3a0bc6bc1bbdd1d341a68
Felix Schütt <felix.schuett@maps4print.com>
2018-07-18 01:30:08 +0200

    Added initial SVG text rendering

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -3,6 +3,6 @@ pub mod button;
 pub mod label;
 
 // Re-export widgets
-pub use self::svg::{Svg, SvgLayerId, SvgLayer, SvgLayerType, SvgWorldPixel, SvgCache};
+pub use self::svg::{Svg, SvgLayerId, SvgLayer, LayerType, SvgStyle, SvgLayerType, SvgWorldPixel, SvgCache};
 pub use self::button::{Button, ButtonContent};
 pub use self::label::Label;
