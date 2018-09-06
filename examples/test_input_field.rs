3f9a1950240c2496855436e866df4184a0094429
Felix Schütt <felix.schuett@maps4print.com>
2018-09-06 21:49:44 +0200

    Add proper classes to TextInput, ignore pseudo-class selectors for now

--- examples/test_input_field.rs
+++ examples/test_input_field.rs
@@ -17,23 +17,14 @@ impl Default for TestCrudApp {
 
 impl Layout for TestCrudApp {
     fn layout(&self, info: WindowInfo<Self>) -> Dom<Self> {
-        Dom::new(NodeType::Div).with_id("parent").with_child(
-            Dom::new(NodeType::Div).with_id("wrapper_1").with_child(
-                TextInput::new()
-                .bind(info.window, &self.text_input, &self)
-                .dom(&self.text_input)))
+        TextInput::new()
+        .bind(info.window, &self.text_input, &self)
+        .dom(&self.text_input)
     }
 }
 
 fn main() {
-    macro_rules! CSS_PATH { () => (concat!(env!("CARGO_MANIFEST_DIR"), "/examples/test_input_field.css")) }
-
-    #[cfg(debug_assertions)]
-    let css = Css::hot_reload(CSS_PATH!()).unwrap();
-    #[cfg(not(debug_assertions))]
-    let css = Css::new_from_str(include_str!(CSS_PATH!())).unwrap();
-
     let mut app = App::new(TestCrudApp::default(), AppConfig::default());
-    app.create_window(WindowCreateOptions::default(), css).unwrap();
+    app.create_window(WindowCreateOptions::default(), Css::native()).unwrap();
     app.run().unwrap();
 }
