38319beff5a2f64cab5339dc3e21f9547615c582
Felix Schütt <felix.schuett@maps4print.com>
2018-09-07 19:42:52 +0200

    Initial IFrame implementation - no rendering yet

--- src/widgets/table_view.rs
+++ src/widgets/table_view.rs
@@ -1,9 +1,10 @@
 //! Table view
 
 use {
-    dom::{Dom, NodeType, IFrameCallback},
+    dom::{Dom, NodeData, NodeType, IFrameCallback},
     traits::Layout,
     window::WindowInfo,
+    default_callbacks::StackCheckedPointer,
 };
 
 #[derive(Debug, Default, Copy, Clone)]
@@ -11,9 +12,14 @@ pub struct TableView {
 
 }
 
-#[derive(Debug, Default, Copy, Clone)]
+#[derive(Debug, Default, Clone)]
 pub struct TableViewOutcome {
+    columns: Vec<TableColumn>,
+}
 
+#[derive(Debug, Default, Clone)]
+pub struct TableColumn {
+    cells: Vec<String>,
 }
 
 impl TableView {
@@ -21,14 +27,25 @@ impl TableView {
         Self { }
     }
 
-    pub fn dom<T: Layout>(&self) -> Dom<T> {
-        Dom::new(NodeType::IFrame(IFrameCallback(render_table)))
+    pub fn dom<T: Layout>(&self, data: &TableViewOutcome, t: &T) -> Dom<T> {
+        Dom::new(NodeType::IFrame((IFrameCallback(render_table_callback), StackCheckedPointer::new(t, data).unwrap())))
     }
 }
 
-fn render_table<T: Layout>(data: &T, info: WindowInfo<T>, width: usize, height: usize) 
--> Dom<T> 
+fn render_table_callback<T: Layout>(ptr: &StackCheckedPointer<T>, info: WindowInfo<T>, width: usize, height: usize)
+-> Dom<T>
 {
-    println!("rendering DOM @ {} x {}", width, height);
+    unsafe { ptr.invoke_mut_iframe(render_table, info, width, height) }
+}
+
+fn render_table<T: Layout>(data: &mut TableViewOutcome, info: WindowInfo<T>, width: usize, height: usize)
+-> Dom<T>
+{
     Dom::new(NodeType::Div).with_class("__azul-native-table-container")
+    .with_child(data.columns.iter().map(|column| {
+        column.cells.iter().map(|cell| {
+            NodeData { node_type: NodeType::Label(cell.clone()), .. Default::default() }
+        }).collect::<Dom<T>>()
+        .with_class("__azul-native-table-column")
+    }).collect())
 }
