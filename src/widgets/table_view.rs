cb43fd846fccc7006ccefe214724a2377c9059f6
Felix Schütt <felix.schuett@maps4print.com>
2018-09-23 14:15:05 +0200

    Table view: Added column names to rendering

--- src/widgets/table_view.rs
+++ src/widgets/table_view.rs
@@ -42,10 +42,69 @@ fn render_table<T: Layout>(data: &mut TableViewOutcome, info: WindowInfo<T>, wid
 -> Dom<T>
 {
     Dom::new(NodeType::Div).with_class("__azul-native-table-container")
+    // Column header with column  - "A", "B", "C"
+    .with_child(
+        (0..data.columns.len())
+        .map(|column_idx| {
+            NodeData { node_type: NodeType::Label(column_name_from_number(column_idx)), .. Default::default() }
+        })
+        .collect::<Dom<T>>()
+        .with_class("__azul-native-table-header")
+    )
     .with_child(data.columns.iter().map(|column| {
         column.cells.iter().map(|cell| {
             NodeData { node_type: NodeType::Label(cell.clone()), .. Default::default() }
         }).collect::<Dom<T>>()
         .with_class("__azul-native-table-column")
     }).collect())
+}
+
+/// Maps an index number to a value, necessary for creating the column name:
+///
+/// ```no_run,ignore
+/// 0   -> A
+/// 25  -> Z
+/// 26  -> AA
+/// 27  -> AB
+/// ```
+///
+/// ... and so on. This implementation is very fast, takes ~50 to 100
+/// nanoseconds for 1 iteration due to almost pure-stack allocated data.
+/// For an explanation of the algorithm with comments, see:
+/// https://github.com/fschutt/street_index/blob/78b935a1303070947c0854b6d01f540ec298c9d5/src/gridconfig.rs#L155-L209
+pub fn column_name_from_number(num: usize) -> String {
+    const ALPHABET_LEN: usize = 26;
+    // usize::MAX is "GKGWBYLWRXTLPP" with a length of 15 characters
+    const MAX_LEN: usize = 15;
+
+    let mut result = [0;MAX_LEN + 1];
+    let mut multiple_of_alphabet = num / ALPHABET_LEN;
+    let mut character_count = 0;
+
+    while multiple_of_alphabet != 0 && character_count < MAX_LEN {
+        let remainder = (multiple_of_alphabet - 1) % ALPHABET_LEN;
+        result[(MAX_LEN - 1) - character_count] = u8_to_char(remainder as u8);
+        character_count += 1;
+        multiple_of_alphabet = (multiple_of_alphabet - 1) / ALPHABET_LEN;
+    }
+
+    result[MAX_LEN] = u8_to_char((num % ALPHABET_LEN) as u8);
+    let zeroed_characters = MAX_LEN.saturating_sub(character_count);
+    let slice = &result[zeroed_characters..];
+    unsafe { ::std::str::from_utf8_unchecked(slice) }.to_string()
+}
+
+#[inline(always)]
+fn u8_to_char(input: u8) -> u8 {
+    'A' as u8 + input
+}
+
+#[test]
+fn test_column_name_from_number() {
+    assert_eq!(column_name_from_number(0), String::from("A"));
+    assert_eq!(column_name_from_number(1), String::from("B"));
+    assert_eq!(column_name_from_number(6), String::from("G"));
+    assert_eq!(column_name_from_number(26), String::from("AA"));
+    assert_eq!(column_name_from_number(27), String::from("AB"));
+    assert_eq!(column_name_from_number(225), String::from("HR"));
 }
