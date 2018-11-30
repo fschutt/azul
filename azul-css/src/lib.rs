291608ce6c8c6fc62b0d29948e01ad95c307232b
Anton Lazarev <antonok35@gmail.com>
2018-11-29 19:54:27 -0500

    Begin splitting css_parser into separate crate

---
+++ azul-css/src/lib.rs
@@ -0,0 +1,21 @@
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/maps4print/azul/master/assets/images/azul_logo_full_min.svg.png",
+    html_favicon_url = "https://raw.githubusercontent.com/maps4print/azul/master/assets/images/favicon.ico",
+)]
+
+#![deny(unused_must_use)]
+#![deny(unreachable_patterns)]
+#![deny(missing_copy_implementations)]
+
+extern crate azul;
+
+extern crate webrender;
+
+extern crate simplecss;
+
+#[macro_use]
+mod macros;
+
+mod css_parser;
+mod css;
+mod dom;
