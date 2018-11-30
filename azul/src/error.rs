28bbb9de991fbc48f9a773cf87bdb3a4d31ad8a6
Anton Lazarev <antonok35@gmail.com>
2018-11-29 21:31:20 -0500

    Remove parsers from core library

--- azul/src/error.rs
+++ azul/src/error.rs
@@ -1,12 +1,8 @@
 pub use app::RuntimeError;
-pub use style::{CssParseError, DynamicCssParseError};
 #[cfg(debug_assertions)]
 pub use style::HotReloadError;
 pub use css_parser::{
-    CssBackgroundParseError, CssBorderParseError, CssStyleBorderRadiusParseError, CssColorParseError,
-    CssDirectionParseError, CssStyleFontFamilyParseError, CssGradientStopParseError, CssImageParseError,
-    CssMetric, CssParsingError, CssShadowParseError, CssShapeParseError, InvalidValueErr,
-    PercentageParseError, PixelParseError,
+    CssMetric, InvalidValueErr,
 };
 pub use font::FontError;
 #[cfg(feature = "image_loading")]
@@ -20,8 +16,7 @@ pub use widgets::errors::*;
 pub use window::WindowCreateError;
 
 #[derive(Debug)]
-pub enum Error<'a> {
-    CssParse(CssParseError<'a>),
+pub enum Error {
     Font(FontError),
     #[cfg(feature = "image_loading")]
     Image(ImageError),
@@ -31,49 +26,42 @@ pub enum Error<'a> {
     HotReload(HotReloadError),
 }
 
-impl<'a> From<CssParseError<'a>> for Error<'a> {
-    fn from(e: CssParseError<'a>) -> Error {
-        Error::CssParse(e)
-    }
-}
-
-impl<'a> From<FontError> for Error<'a> {
-    fn from(e: FontError) -> Error<'a> {
+impl From<FontError> for Error {
+    fn from(e: FontError) -> Error {
         Error::Font(e)
     }
 }
 
 #[cfg(feature = "image_loading")]
-impl<'a> From<ImageError> for Error<'a> {
-    fn from(e: ImageError) -> Error<'a> {
+impl From<ImageError> for Error {
+    fn from(e: ImageError) -> Error {
         Error::Image(e)
     }
 }
 
-impl<'a> From<ClipboardError> for Error<'a> {
-    fn from(e: ClipboardError) -> Error<'a> {
+impl From<ClipboardError> for Error {
+    fn from(e: ClipboardError) -> Error {
         Error::Clipboard(e)
     }
 }
 
-impl<'a> From<WindowCreateError> for Error<'a> {
-    fn from(e: WindowCreateError) -> Error<'a> {
+impl From<WindowCreateError> for Error {
+    fn from(e: WindowCreateError) -> Error {
         Error::WindowCreate(e)
     }
 }
 
 #[cfg(debug_assertions)]
-impl<'a> From<HotReloadError> for Error<'a> {
-    fn from(e: HotReloadError) -> Error<'a> {
+impl From<HotReloadError> for Error {
+    fn from(e: HotReloadError) -> Error {
         Error::HotReload(e)
     }
 }
 
 use std::fmt;
-impl<'a> fmt::Display for Error<'a> {
+impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::CssParse(e) => write!(f, "[CSS parsing error] {}", e),
             Error::Font(e) => write!(f, "[Font error] {}", e),
             #[cfg(feature = "image_loading")]
             Error::Image(e) => write!(f, "[Image error] {}", e),
