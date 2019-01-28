8ffad16fa8496ee6bff621919787339e1a9bce37
Felix Schütt <felix.schuett@maps4print.com>
2019-01-28 04:38:10 +0100

    Fixed TextInput to react to focus events instead of hover events
    Previously, a user had to hover the cursor over the text field in
    order to enter text. This is now fixed, text fields can be focused
    and unfocused by clicking on them.

--- examples/text_input.rs
+++ examples/text_input.rs
@@ -3,6 +3,27 @@ extern crate azul;
 use azul::prelude::*;
 use azul::widgets::text_input::*;
 
+const CSS: &str = "
+#text_input_1 {
+    border-radius: 2px;
+    width: 200px;
+    height: 20px;
+    border: 2px solid transparent;
+    background-color: #ccc;
+    position: absolute;
+    top: 40px;
+    left: 40px;
+}
+
+#text_input_1 p {
+    font-size: 10px;
+}
+
+#text_input_1:focus {
+    border: 2px solid  #80ff80;
+}
+";
+
 struct TestCrudApp {
     text_input: TextInputState,
 }
@@ -20,10 +41,11 @@ impl Layout for TestCrudApp {
         TextInput::new()
         .bind(info.window, &self.text_input, &self)
         .dom(&self.text_input)
+        .with_id("text_input_1")
     }
 }
 
 fn main() {
     let app = App::new(TestCrudApp::default(), AppConfig::default());
-    app.run(Window::new(WindowCreateOptions::default(), css::native()).unwrap()).unwrap();
+    app.run(Window::new(WindowCreateOptions::default(), css::override_native(CSS).unwrap()).unwrap()).unwrap();
 }
