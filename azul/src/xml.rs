f62b4b9a265aaf2a5863baafdd9c61d42af56368
Felix Schütt <felix.schuett@maps4print.com>
2019-02-16 23:52:51 +0100

    Implemented full XML parser (for loading DOMs from XML files)

--- azul/src/xml.rs
+++ azul/src/xml.rs
@@ -45,6 +45,26 @@ pub struct XmlNode {
     pub text: Option<String>,
 }
 
+impl XmlNode {
+
+    pub fn new(tag_name: &str) -> Self {
+        Self {
+            tag_name: tag_name.into(),
+            .. Default::default()
+        }
+    }
+
+    pub fn with_attribute(mut self, key: &str, value: &str) -> Self {
+        self.attributes.insert(key.into(), value.into());
+        self
+    }
+
+    pub fn with_children(mut self, children: Vec<XmlNode>) -> Self {
+        self.children = children;
+        self
+    }
+}
+
 pub struct XmlComponentMap<T: Layout> {
     components: BTreeMap<String, Box<XmlComponent<T>>>,
     callbacks: BTreeMap<String, Callback<T>>,
@@ -56,7 +76,6 @@ impl<T: Layout> XmlComponentMap<T> {
     }
 }
 
-#[derive(Debug)]
 pub enum XmlParseError {
     /// No `<app></app>` root component present
     NoRootComponent,
@@ -69,6 +88,11 @@ pub enum XmlParseError {
     ParseError(XmlError),
     /// Invalid hierarchy close tags, i.e `<app></p></app>`
     MalformedHierarchy(String, String),
+}
+impl fmt::Debug for XmlParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
 }
 
 impl fmt::Display for XmlParseError {
@@ -85,67 +109,76 @@ impl fmt::Display for XmlParseError {
 
 /// Parses the XML string into an XML tree, returns
 /// the root `<app></app>` node, with the children attached to it.
-pub fn parse_xml(xml: &str) -> Result<XmlNode, XmlParseError> {
+///
+/// Since the XML allows multiple root nodes, this function returns
+/// a `Vec<XmlNode>` - which are the "root" nodes, containing all their
+/// children recursively.
+///
+/// # Example
+///
+/// ```rust
+/// # use azul::xml::{XmlNode, parse_xml};
+/// assert_eq!(
+///     parse_xml("<app><p /><div id='thing' /></app>").unwrap(),
+///     vec![
+///          XmlNode::new("app").with_children(vec![
+///             XmlNode::new("p"),
+///             XmlNode::new("div").with_attribute("id", "thing"),
+///         ])
+///     ]
+/// )
+/// ```
+pub fn parse_xml_string(xml: &str) -> Result<Vec<XmlNode>, XmlParseError> {
 
     use xmlparser::Token::*;
     use xmlparser::ElementEnd::*;
     use self::XmlParseError::*;
 
-    let mut root_node = XmlNode {
-        tag_name: "app".into(),
-        attributes: BTreeMap::new(),
-        children: Vec::new(),
-        text: None,
-    };
+    let mut root_node = XmlNode::default();
 
     let mut tokenizer = Tokenizer::from(xml);
     tokenizer.enable_fragment_mode();
 
-    let mut stack = Vec::new();
+    // In order to insert where the item is, let's say
+    // [0 -> 1st element, 5th-element -> node]
+    // we need to trach the index of the item in the parent.
+    let mut current_hierarchy: Vec<usize> = Vec::new();
 
     for token in tokenizer {
 
         let token = token.map_err(|e| ParseError(e))?;
         match token {
             ElementStart(_, open_value) => {
-                stack.push(XmlNode {
-                    tag_name: open_value.to_str().into(),
-                    attributes: BTreeMap::new(),
-                    children: Vec::new(),
-                    text: None,
-                });
-            },
-            ElementEnd(Empty) => {
-                if let Some(top) = stack.pop() {
-                    if stack.is_empty() {
-                        // element is a top-level element
-                        root_node.children.push(top);
-                    } else {
-                        // element has a parent, this is hard
-                    }
+                if let Some(current_parent) = get_item(&current_hierarchy, &mut root_node) {
+                    let children_len = current_parent.children.len();
+                    current_parent.children.push(XmlNode {
+                        tag_name: open_value.to_str().into(),
+                        attributes: BTreeMap::new(),
+                        children: Vec::new(),
+                        text: None,
+                    });
+                    current_hierarchy.push(children_len);
                 }
             },
+            ElementEnd(Empty) => {
+                current_hierarchy.pop();
+            },
             ElementEnd(Close(_, close_value)) => {
-                if let Some(last) = stack.pop() {
-                    let close_value = close_value.to_str();
+                let close_value = close_value.to_str();
+                if let Some(last) = get_item(&current_hierarchy, &mut root_node) {
                     if last.tag_name != close_value {
                         return Err(MalformedHierarchy(close_value.into(), last.tag_name.clone()));
                     }
-                    if stack.is_empty() {
-                        // element is a top-level element
-                        root_node.children.push(last);
-                    } else {
-                        // element has a parent, this is hard
-                    }
                 }
+                current_hierarchy.pop();
             },
             Attribute((_, key), value) => {
-                if let Some(last) = stack.last_mut() {
+                if let Some(last) = get_item(&current_hierarchy, &mut root_node) {
                     last.attributes.insert(key.to_str().into(), value.to_str().into());
                 }
             },
             Text(t) => {
-                if let Some(last) = stack.last_mut() {
+                if let Some(last) = get_item(&current_hierarchy, &mut root_node) {
                     if let Some(s) = last.text.as_mut() {
                         s.push_str(t.to_str());
                     }
@@ -158,7 +191,75 @@ pub fn parse_xml(xml: &str) -> Result<XmlNode, XmlParseError> {
         }
     }
 
-    Ok(root_node)
+    Ok(root_node.children)
+}
+
+/// Given a root node, traverses along the hierarchy, and returns a
+/// mutable reference to a child of the node if
+fn get_item<'a>(hierarchy: &[usize], root_node: &'a mut XmlNode) -> Option<&'a mut XmlNode> {
+    let mut current_node = &*root_node;
+    let mut iter = hierarchy.iter();
+
+    while let Some(item) = iter.next() {
+        current_node = current_node.children.get(*item).as_ref()?;
+    }
+
+    // Safe because we only ever have one mutable reference, but
+    // the borrow checker doesn't allow recursive mutable borrowing
+    let node_ptr = current_node as *const XmlNode;
+    let mut_node_ptr = node_ptr as *mut XmlNode;
+    Some(unsafe { &mut *mut_node_ptr })
+}
+
+#[test]
+fn test_xml_get_item() {
+
+    // <a>
+    //     <b/>
+    //     <c/>
+    //     <d/>
+    //     <e/>
+    // </a>
+    // <f>
+    //     <g>
+    //         <h/>
+    //     </g>
+    //     <i/>
+    // </f>
+    // <j/>
+
+    let mut tree = XmlNode::new("component")
+    .with_children(vec![
+        XmlNode::new("a")
+        .with_children(vec![
+            XmlNode::new("b"),
+            XmlNode::new("c"),
+            XmlNode::new("d"),
+            XmlNode::new("e"),
+        ]),
+        XmlNode::new("f")
+        .with_children(vec![
+            XmlNode::new("g")
+            .with_children(vec![XmlNode::new("h")]),
+            XmlNode::new("i"),
+        ]),
+        XmlNode::new("j"),
+    ]);
+
+    assert_eq!(&get_item(&[], &mut tree).unwrap().tag_name, "component");
+    assert_eq!(&get_item(&[0], &mut tree).unwrap().tag_name, "a");
+    assert_eq!(&get_item(&[0, 0], &mut tree).unwrap().tag_name, "b");
+    assert_eq!(&get_item(&[0, 1], &mut tree).unwrap().tag_name, "c");
+    assert_eq!(&get_item(&[0, 2], &mut tree).unwrap().tag_name, "d");
+    assert_eq!(&get_item(&[0, 3], &mut tree).unwrap().tag_name, "e");
+    assert_eq!(&get_item(&[1], &mut tree).unwrap().tag_name, "f");
+    assert_eq!(&get_item(&[1, 0], &mut tree).unwrap().tag_name, "g");
+    assert_eq!(&get_item(&[1, 0, 0], &mut tree).unwrap().tag_name, "h");
+    assert_eq!(&get_item(&[1, 1], &mut tree).unwrap().tag_name, "i");
+    assert_eq!(&get_item(&[2], &mut tree).unwrap().tag_name, "j");
+
+    assert_eq!(get_item(&[123213], &mut tree), None);
+    assert_eq!(get_item(&[0, 1, 2], &mut tree), None);
 }
 
 /*
