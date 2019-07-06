df47bfdfbbcf72e1f430e8170449b8667a1c91a2
Trangar <victor.koenders@gmail.com>
2019-07-06 09:10:35 +0200

    Fixed travis build - Rust 1.31 does not support:   - creating `Self` instances (stabilized in 1.32)   - importing dependencies `as _` (stabilized in 1.33) - Pinned linux on ubuntu 16, because servo-fontconfig-sys requires fontconfig=2.11.1 and that's not available on ubuntu 14 - Enabled osx but allowed it to fail so people can work on that - Fixed an issue in an example

--- azul/app.rs
+++ azul/app.rs
@@ -31,12 +31,12 @@ use {
 use azul_core::{
     ui_solver::ScrolledNodes,
     window::WindowId,
+    ui_description::UiDescription,
 };
 pub use app_resources::AppResources;
 #[cfg(not(test))]
 use azul_core::{
     window::FakeWindow,
-    ui_description::UiDescription,
 };
 #[cfg(not(test))]
 use window::{ FakeDisplay, WindowCreateError, WindowCreateOptions };
