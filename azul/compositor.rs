62194aef44a8d57d3d009868255963a0faf8214c
Felix Schütt <felix.schuett@maps4print.com>
2019-12-07 13:09:51 +0100

    Added option to allocate / deallocate OsMesaContext
    This is a preparation for a future integration of OsMesa into the
    azul-desktop renderer

--- azul/compositor.rs
+++ azul/compositor.rs
@@ -36,3 +36,51 @@ impl WrExternalImageHandler for Compositor {
         // Since the renderer is currently single-threaded, there is nothing to do here
     }
 }
+
+// see: https://github.com/servo/rust-offscreen-rendering-context/pull/65/files
+#[cfg(feature="osmesa")] 
+mod osmesa {
+
+    use osmesa_sys::OSMesaContext;
+
+    pub use platform::{OSMesaContext, OSMesaContextHandle};
+
+    pub struct OSMesaContext {
+        buffer: Vec<u8>,
+        context: osmesa_sys::OSMesaContext,
+    }
+
+    pub struct OSMesaContextHandle(osmesa_sys::OSMesaContext);
+
+    unsafe impl Send for OSMesaContextHandle {}
+
+    #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub enum OsMesaCreateError {
+        // OSMesaCreateContext returned a null pointer (should never happen)
+        NullContext,
+    }
+
+    impl OSMesaContext {
+
+        /// Returns a memory-backed RGBA-buffer
+        pub fn new(width: usize, height: usize, shared_ctxt: Option<OSMesaContext>) -> Result<Self, OsMesaCreateError> {
+
+            let shared = shared_ctxt.unwrap_or(ptr::null_mut());
+
+            let context = unsafe { osmesa_sys::OSMesaCreateContext(osmesa_sys::OSMESA_RGBA, shared) };
+
+            if context.is_null() { return Err(OsMesaCreateError::NullContext); }
+
+            Ok(OSMesaContext {
+                buffer: vec![0u8; width * height * 4],
+                context: context,
+            })
+        }
+    }
+
+    impl Drop for OSMesaContext {
+        fn drop(&mut self) {
+            unsafe { osmesa_sys::OSMesaDestroyContext(self.context) }
+        }
+    }
+}
