6e672e26d409613b9cb9ee2de1affe1464149a87
Felix Schütt <felix.schuett@maps4print.com>
2020-05-11 20:57:43 +0200

    Publish azulc

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@ cd "./cargo/azul-css" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azul-css-parser" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azul-core" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azul-text-layout" && cargo check && cargo publish && cd "../.." &&
-cd "./cargo/azul-layout" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azulc" && cargo check && cargo publish && cd "../.." &&
+cd "./cargo/azul-layout" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azul-desktop" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azul-web" && cargo check && cargo publish && cd "../.." &&
 cd "./cargo/azul-dll" && cargo check && cargo publish && cd "../.." &&
