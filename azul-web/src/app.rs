54358a865e32ea9ccf46a82d15cf359f357527d1
Felix Schütt <felix.schuett@maps4print.com>
2020-05-11 23:08:48 +0200

    Update azul-web to new dll API

--- azul-web/src/app.rs
+++ azul-web/src/app.rs
@@ -1,6 +1,5 @@
 use azul_core::window::WindowCreateOptions;
 use azul_core::callbacks::LayoutCallback;
-use azul_core::traits::Layout;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, Hash)]
 pub struct AppConfig {
@@ -14,22 +13,16 @@ impl Default for AppConfig {
 pub struct App<T> {
     pub data: T,
     pub config: AppConfig,
-    pub callback: LayoutCallback<T>,
-}
-
-impl<T: Layout> App<T> {
-    pub fn new(data: T, config: AppConfig) -> Self {
-        Self::new_from_callback(data, config, T::layout)
-    }
+    pub callback: LayoutCallback,
 }
 
 impl<T> App<T> {
 
-    pub fn new_from_callback(data: T, config: AppConfig, callback: LayoutCallback<T>) -> Self {
+    pub fn new(data: T, config: AppConfig, callback: LayoutCallback) -> Self {
         Self { data, config, callback }
     }
 
-    pub fn run(self, _window: WindowCreateOptions<T>) -> ! {
+    pub fn run(self, _window: WindowCreateOptions) -> ! {
         loop { }
     }
 }
