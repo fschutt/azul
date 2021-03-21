5835430c633c2ec6a060d66eb3c7065aeea9073d
Felix Schütt <felix.schuett@maps4print.com>
2021-03-21 22:32:04 +0100

    Compile freetype statically on Linxu again

--- api/rust/build.rs
+++ api/rust/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // dynamically link azul.dll
     #[cfg(not(feature = "link_static"))] {
-        println!("cargo:rustc-link-search={}", env!("AZUL_INSTALL_DIR"));
+        println!("cargo:rustc-link-search={}", env!("AZUL_LINK_PATH")); /* path to folder with azul.dll / libazul.so */
     }
 }
