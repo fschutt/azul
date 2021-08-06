9ca5afa09d0fc3b49392257cc347dab135fb74f5
fschutt <felix.schuett@maps4print.com>
2021-08-06 16:01:06 +0200

    Added benchmark for SVG drawing

--- examples/rust/svg.rs
+++ examples/rust/svg.rs
@@ -30,20 +30,42 @@ extern "C" fn layout(data: &mut RefAny, _: LayoutCallbackInfo) -> StyledDom {
 
 fn main() {
 
+    let mut start = std::time::Instant::now();
+
+    // parse the SVG
     let svg = match Svg::from_string(SVG_STRING.clone(), SvgParseOptions::default()) {
         ResultSvgSvgParseError::Ok(o) => o,
         ResultSvgSvgParseError::Err(e) => { return; },
     };
 
+    let end = std::time::Instant::now();
+    let parse_time = end - start;
+    start = end;
+
+    // render the SVG
     let rendered_svg = match svg.render(SvgRenderOptions::default()) {
         OptionRawImage::Some(s) => s,
         OptionRawImage::None => { return; },
     };
 
+    let end = std::time::Instant::now();
+    let render_time = end - start;
+    start = end;
+
+    // ---- convert the rendered image to a webrender-compatible format
     let image_ref = match ImageRef::raw_image(rendered_svg) {
         OptionImageRef::Some(s) => s,
         OptionImageRef::None => { return; },
     };
+
+    let end = std::time::Instant::now();
+    let recode_time = end - start;
+    start = end;
+
+    MsgBox::info(format!(
+        "Ok - Svg file rendered!\r\n\r\nparsing took: {:?}\r\nrendering took: {:?}\r\nencoding took: {:?}\r\n",
+        parse_time, render_time, recode_time).into()
+    );
 
     let data = RefAny::new(MyAppData { svg: image_ref });
     let app = App::new(data, AppConfig::new(LayoutSolver::Default));
