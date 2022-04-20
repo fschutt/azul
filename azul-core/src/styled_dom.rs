0cf3de7906e5836739bc9034d57541c6332c9e25
fschutt <felix.schuett@maps4print.com>
2022-04-20 17:32:49 +0200

    Add shim DOM for scroll bar wrapper

--- azul-core/src/styled_dom.rs
+++ azul-core/src/styled_dom.rs
@@ -1536,7 +1536,11 @@ impl StyledDom {
     }
 
     /// Inject scroll bar DIVs with relevant event handlers into the DOM
+    ///
+    /// This function essentially takes
     pub fn inject_scroll_bars(&mut self) {
+
+        use azul_css_parser::CssApiWrapper;
 
         // allocate 14 nodes for every node
         //
@@ -1557,6 +1561,90 @@ impl StyledDom {
         // 14:          |- self.root
         //                  |- ... self.children
 
+        let dom_to_inject = Dom::div()
+        // .with_class("__azul-native-scroll-root-component".into())
+        .with_inline_style("display:flex; flex-grow:1; flex-direction:column;".into())
+        .with_children(vec![
+
+            Dom::div()
+            // .with_class("__azul-native-scroll-vertical-container".into())
+            .with_inline_style("display:flex; flex-grow:1; flex-direction:column-reverse;".into())
+            .with_children(vec![
+
+                Dom::div()
+                // .with_class("__azul-native-scroll-horizontal-scrollbar".into())
+                .with_inline_style("display:flex; flex-grow:1; flex-direction:row; height:15px; background:grey;".into())
+                .with_children(vec![
+                    Dom::div(),
+                    // .with_class("__azul-native-scroll-horizontal-scrollbar-track-left".into()),
+                    Dom::div()
+                    // .with_class("__azul-native-scroll-horizontal-scrollbar-track-middle".into())
+                    .with_children(vec![
+                        Dom::div()
+                        // .with_class("__azul-native-scroll-horizontal-scrollbar-track-thumb".into())
+                    ].into()),
+                    Dom::div()
+                    // .with_class("__azul-native-scroll-horizontal-scrollbar-track-right".into()),
+                ].into()),
+
+                Dom::div()
+                // .with_class("__azul-native-scroll-content-container-1".into())
+                .with_inline_style("display:flex; flex-grow:1; flex-direction:row-reverse;".into())
+                .with_children(vec![
+
+                    Dom::div()
+                    // .with_class("__azul-native-scroll-vertical-scrollbar".into())
+                    .with_inline_style("display:flex; flex-grow:1; flex-direction:column; width:15px; background:grey;".into())
+                    .with_children(vec![
+                       Dom::div(),
+                       // .with_class("__azul-native-scroll-vertical-scrollbar-track-top".into()),
+                       Dom::div()
+                       // .with_class("__azul-native-scroll-vertical-scrollbar-track-middle".into())
+                       .with_children(vec![
+                           Dom::div()
+                           // .with_class("__azul-native-scroll-vertical-scrollbar-track-thumb".into())
+                       ].into()),
+                       Dom::div()
+                       // .with_class("__azul-native-scroll-vertical-scrollbar-track-bottom".into()),
+                    ].into()),
+
+                    Dom::div()
+                    // .with_class("__azul-native-scroll-content-container-1".into())
+                    .with_inline_style("display:flex; flex-grow:1; flex-direction:column;".into())
+                    .with_children(vec![
+                        Dom::div() // <- this div is where the new children will be injected into
+                    ].into())
+                ].into())
+            ].into())
+        ].into())
+        .style(CssApiWrapper::empty());
+
+        // allocate new nodes
+        let nodes_to_allocate = self.node_data.len()
+            + (self.non_leaf_nodes.len() * dom_to_inject.node_data.len());
+
+        // pre-allocate a new DOM tree with self.nodes.len() * dom_to_inject.nodes.len() nodes
+
+        let mut new_styled_dom = StyledDom {
+            root: self.root,
+            node_hierarchy: vec![NodeHierarchyItem::zeroed();nodes_to_allocate].into(),
+            node_data: vec![NodeData::const_div();nodes_to_allocate].into(),
+            styled_nodes: vec![StyledNode::default();nodes_to_allocate].into(),
+            cascade_info: vec![CascadeInfo::default();nodes_to_allocate],
+            nodes_with_window_callbacks: self.nodes_with_window_callbacks.clone(),
+            nodes_with_not_callbacks: self.nodes_with_not_callbacks.clone(),
+            nodes_with_datasets: self.nodes_with_datasets.clone(),
+            tag_ids_to_node_ids: self.tag_ids_to_node_ids.clone(),
+            non_leaf_nodes: self.non_leaf_nodes.clone(),
+            css_property_cache: self.css_property_cache.clone(),
+        };
+
+        // inject self.root as the nth node
+        let inject_as_id = 0;
+
+        println!("inject scroll bars:\r\n{}", dom_to_inject.get_html_string("", "", true));
+
+        // *self = new_styled_dom;
     }
 
     /// Inject a menu bar into the root component
--- examples/rust/fontdebug.rs
+++ examples/rust/fontdebug.rs
@@ -8,10 +8,80 @@ struct DataModel {
 
 extern "C"
 fn myLayoutFunc(data: &mut RefAny, _: &mut LayoutCallbackInfo) -> StyledDom {
-    Dom::body().with_child(
+
+    get_dom()
+
+    /*Dom::body().with_child(
         Dom::text("Test".into())
         .with_inline_style("font-size: 50px;".into())
-    ).style(Css::empty())
+    ).style(Css::empty())*/
+}
+
+fn get_dom() -> StyledDom {
+
+    Dom::body()
+    .with_inline_style("display:flex; flex-grow:1; flex-direction:column;".into())
+    .with_children(vec![
+       Dom::div()
+       // .with_class("__azul-native-scroll-root-component".into())
+       .with_inline_style("display:flex; flex-grow:1; flex-direction:column;".into())
+       .with_children(vec![
+
+           Dom::div()
+           // .with_class("__azul-native-scroll-vertical-container".into())
+           .with_inline_style("display:flex; flex-grow:1; flex-direction:column-reverse;".into())
+           .with_children(vec![
+
+               Dom::div()
+               // .with_class("__azul-native-scroll-horizontal-scrollbar".into())
+               .with_inline_style("display:flex; flex-grow:1; flex-direction:row; height:15px; max-height:15px; background:grey;".into())
+               .with_children(vec![
+                   Dom::div(),
+                   // .with_class("__azul-native-scroll-horizontal-scrollbar-track-left".into()),
+                   Dom::div()
+                   // .with_class("__azul-native-scroll-horizontal-scrollbar-track-middle".into())
+                   .with_children(vec![
+                       Dom::div()
+                       // .with_class("__azul-native-scroll-horizontal-scrollbar-track-thumb".into())
+                   ].into()),
+                   Dom::div()
+                   // .with_class("__azul-native-scroll-horizontal-scrollbar-track-right".into()),
+               ].into()),
+
+               Dom::div()
+               // .with_class("__azul-native-scroll-content-container-1".into())
+               .with_inline_style("display:flex; flex-grow:1; flex-direction:row-reverse;".into())
+               .with_children(vec![
+
+                   Dom::div()
+                   // .with_class("__azul-native-scroll-vertical-scrollbar".into())
+                   .with_inline_style("display:flex; flex-grow:1; flex-direction:column; width:15px; max-width:15px; background:grey;".into())
+                   .with_children(vec![
+                      Dom::div(),
+                      // .with_class("__azul-native-scroll-vertical-scrollbar-track-top".into()),
+                      Dom::div()
+                      // .with_class("__azul-native-scroll-vertical-scrollbar-track-middle".into())
+                      .with_children(vec![
+                          Dom::div()
+                          // .with_class("__azul-native-scroll-vertical-scrollbar-track-thumb".into())
+                      ].into()),
+                      Dom::div()
+                      // .with_class("__azul-native-scroll-vertical-scrollbar-track-bottom".into()),
+                   ].into()),
+
+                   Dom::div()
+                   // .with_class("__azul-native-scroll-content-container-1".into())
+                   .with_inline_style("display:flex; flex-grow:1; flex-direction:column;".into())
+                   .with_children(vec![
+                       Dom::div() // <- this div is where the new children will be injected into
+                       .with_inline_style("display:block;width:50px;height:50px;background:red;".into())
+                   ].into())
+               ].into())
+           ].into())
+       ].into())
+    ].into())
+
+    .style(Css::empty())
 }
 
 fn main() {
@@ -19,4 +89,6 @@ fn main() {
     let app = App::new(RefAny::new(data), AppConfig::new(LayoutSolver::Default));
     let mut window = WindowCreateOptions::new(myLayoutFunc);
     app.run(window);
+
+    println!("inject scroll bars:\r\n{}", get_dom().get_html_string());
 }
